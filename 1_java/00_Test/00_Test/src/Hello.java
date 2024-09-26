12957d85883182b5b5dd7490eeb10ee10fa2a87f
mindnr0806 <alsdnr6672@naver.com>
2024-09-26 18:43:41 +0900

    Add files via upload

---
+++ 1_java/00_Test/00_Test/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	//	프로그램 실행해주는 메소드 : main
+	public static void main(String[] args) {  
+
+		System.out.println("bye!!!");
+		// Q. 문자열타입의(stringe)을 저장할 변수를 선언해봅시다.
+		// * 변수 선언 : 자료형 변수명"
+		String name;
+			
+		// Q, 실수혈 타입(DOUBLE)의 키(HELGHT)를 저장할 변수를 선언해봄시다.
+		
+	}
+}
---
+++ 1_java/00_Test/00_Test/src/com/kh/sports/Sports.java
@@ -0,0 +1,11 @@
+package com.kh.sports;
+
+class Sports {
+	 // 추상 메소드 play 선언
+	 public void play() {}
+	 
+	 // 메소드 prepare 선언
+	 public void prepare() {
+	   System.out.println("Preparing for the game..");
+	 } 
+}
