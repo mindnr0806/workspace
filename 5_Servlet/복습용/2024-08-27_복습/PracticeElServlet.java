243b645b30f04de3cf76e466337403d09beae2ea
mindnr0806 <alsdnr6672@naver.com>
2024-09-26 18:41:05 +0900

    Add files via upload

---
+++ 5_Servlet/복습용/2024-08-27_복습/PracticeElServlet.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PracticeElServlet
+ */
+@WebServlet("/practice.el")
+public class PracticeElServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PracticeElServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// TODO: 1부터 100 사이의 랜덤값 2개를 추출하여 request scope에 저장
+
+		
+		// TODO: practice_el.jsp 페이지로 응답
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
